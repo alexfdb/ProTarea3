742b41a6fcdef15dc2190709a141d689ff5b5498
Alexander Faustino Diaz Bautista <AlexanderFaustinoDiazBautista@Gmail.com>
2024-12-11 12:02:45 +0000

    Realizo los ejercicios del 1 al 5

--- tarea4alex/src/main/java/es/ies/puerto/Ejercicio4.java
+++ tarea4alex/src/main/java/es/ies/puerto/Ejercicio4.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Vector;
+import java.util.stream.Collectors;
 /**
  * @author alexfdb
  * @version 1.0.0
@@ -13,15 +14,19 @@ import java.util.Vector;
 public class Ejercicio4 {
     static Vector<Integer> numeros1 = new Vector<>(Arrays.asList(1, 2, 3, 4, 5));
     public static void main(String[] args) {
+        Vector<Integer> numeros2 = new Vector<>(copiarElementosPares(numeros1));
+        System.out.println(numeros2);
     }
 
-    public static Boolean copiarElementosPares(List<Integer> lista) {
-        if(lista == null || lista.isEmpty()) return false;
-        List<String> listaPares = new ArrayList<>();
-        for (Integer palabra : lista) {
-            if(lista.get(palabra) % 2 == 0){
-            }
-        }
-        return true;
+    /**
+     * Copia en una nueva lista solo los números pares si los hubiera.
+     * @param lista de la que copiar los números pares.
+     * @return una nueva lista solo con los números pares de la lista original.
+     */
+    public static List<Integer> copiarElementosPares(List<Integer> lista) {
+        if(lista == null || lista.isEmpty()) return new ArrayList<>();
+        return lista.stream()
+                    .filter(numero -> numero % 2 == 0)
+                    .collect(Collectors.toList());
     }
 }
--- tarea4alex/src/main/java/es/ies/puerto/Ejercicio5.java
+++ tarea4alex/src/main/java/es/ies/puerto/Ejercicio5.java
@@ -1,7 +1,30 @@
 package es.ies.puerto;
-
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
+/**
+ * @author alexfdb
+ * @version 1.0.0
+ * Descripción: Ordenar un vector con números aleatorios en orden ascendente.
+ * Métodos a utilizar: add(), sort() (de Collections).
+ * Resultado esperado: Antes: [4, 2, 5, 1, 3]. Después: [1, 2, 3, 4, 5].
+ */
 public class Ejercicio5 {
+    static Vector<Integer> numeros = new Vector<>(Arrays.asList(4, 2, 5, 1, 3));
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ordenarVector(numeros);
+        System.out.println(numeros);
+    }
+
+    /**
+     * Ordena una lista si esta no es null ni esta vacía.
+     * @param lista a ordenar.
+     * @return true si la lista fue ordenada.
+     */
+    public static Boolean ordenarVector(List<Integer> lista) {
+        if(lista == null || lista.isEmpty()) return false;
+        Collections.sort(lista);
+        return true;
     }
 }
